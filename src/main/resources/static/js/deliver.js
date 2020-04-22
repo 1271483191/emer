fc8fe2789e29f254085d85f085f37ef3046b012f
yinwenyao <331026596@qq.com>
2020-04-22 14:24:20 +0800

    配送修改

---
+++ src/main/resources/static/js/deliver.js
@@ -0,0 +1,146 @@
+/**
+ * Created by yinwenyao on 20/4/22.
+ */
+
+$(document).ready(function(){
+    // $.post("deliverBar",function(data){deliverBar(data);});
+    deliverBar();
+});
+
+function deliverBar(){
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('deliverBar'));
+
+
+
+    var dataMap = {};
+    function dataFormatter(obj) {
+        var pList = ['北京','天津','河北','山西','内蒙古','辽宁','吉林','黑龙江','上海','江苏','浙江','安徽','福建','江西','山东','河南','湖北','湖南','广东','广西','海南','重庆','四川','贵州','云南','西藏','陕西','甘肃','青海','宁夏','新疆'];
+        var temp;
+        var max = 0;
+        var sum = 0;
+        temp = obj[2020];
+        for (var i = 0, l = temp.length; i < l; i++) {
+            max = Math.max(max, temp[i]);
+            sum += temp[i];
+            obj[2020][i] = {
+                name: pList[i],
+                value: temp[i]
+            };
+        }
+        obj[2020 + 'max'] = Math.floor(max / 100) * 100;
+        obj[2020 + 'sum'] = sum;
+        return obj;
+    }
+
+
+    dataMap.dataPI = dataFormatter({
+        //max : 4000,
+        2020:[136.27,159.72,2905.73,641.42,1306.3,1915.57,1277.44,1701.5,124.94,3064.78,1583.04,2015.31,1612.24,1391.07,3973.85,3512.24,2569.3,2768.03,2665.2,2047.23,659.23,844.52,2983.51,726.22,1411.01,74.47,1220.9,678.75,155.08,184.14,1139.03],
+    });
+
+    dataMap.dataSI = dataFormatter({
+        //max : 26600,
+        2020:[3752.48,5928.32,13126.86,6635.26,8037.69,12152.15,5611.48,5962.41,7927.89,25203.28,16555.58,8309.38,9069.2,6390.55,24017.11,15427.08,9815.94,9361.99,26447.38,5675.32,714.5,5543.04,11029.13,2194.33,3780.32,208.79,6935.59,2377.83,975.18,1056.15,3225.9],
+    });
+
+    dataMap.dataTI = dataFormatter({
+        //max : 25000,
+        2020:[12363.18,5219.24,8483.17,3960.87,5015.89,8158.98,3679.91,4918.09,11142.86,20842.21,14180.23,4975.96,6878.74,3921.2,17370.89,7991.72,7247.02,7539.54,24097.7,3998.33,1148.93,3623.81,7014.04,2781.29,3701.79,322.57,4355.81,1963.79,540.18,861.92,2245.12],
+    });
+    var option = {
+        baseOption: {
+            timeline: {
+                axisType: 'category',
+                // realtime: false,
+                // loop: false,
+                autoPlay: true,
+                // currentIndex: 2,
+                playInterval: 1000,
+                // controlStyle: {
+                //     position: 'left'
+                // },
+                data: [
+                    '2020-01-01'
+                ],
+                label: {
+                    formatter : function(s) {
+                        return (new Date(s)).getFullYear();
+                    }
+                }
+            },
+            title: {
+                subtext: '数据来自国家粮食和物质储备局'
+            },
+            legend: {
+                left: 'right',
+                data: ['日入库量', '日销量', '日库存', ]
+            },
+            calculable : true,
+            grid: {
+                top: 80,
+                bottom: 100,
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'shadow',
+                        label: {
+                            show: true,
+                            formatter: function (params) {
+                                return params.value.replace('\n', '');
+                            }
+                        }
+                    }
+                }
+            },
+            xAxis: [
+                {
+                    'type':'category',
+                    'axisLabel':{'interval':0},
+                    'data':[
+                        '北京','\n天津','河北','\n山西','内蒙古','\n辽宁','吉林','\n黑龙江',
+                        '上海','\n江苏','浙江','\n安徽','福建','\n江西','山东','\n河南',
+                        '湖北','\n湖南','广东','\n广西','海南','\n重庆','四川','\n贵州',
+                        '云南','\n西藏','陕西','\n甘肃','青海','\n宁夏','新疆'
+                    ],
+                    splitLine: {show: false}
+                }
+            ],
+            yAxis: [
+                {
+                    type: 'value',
+                    name: '指标（吨）'
+                }
+            ],
+            series: [
+                {name: '日入库量', type: 'bar'},
+                {name: '日销量', type: 'bar'},
+                {name: '日库存', type: 'bar'},
+                {
+                    name: '日入库量',
+                    type: 'pie',
+                    center: ['75%', '35%'],
+                    radius: '28%',
+                    z: 100
+                }
+            ]
+        },
+        options: [
+            {
+                title: {text: '2020全国应急保障企业指标'},
+                series: [
+                    {data: dataMap.dataPI['2020']},
+                    {data: dataMap.dataSI['2020']},
+                    {data: dataMap.dataTI['2020']},
+                    {data: [
+                        {name: '日入库量', value: dataMap.dataPI['2020sum']},
+                        {name: '日销量', value: dataMap.dataSI['2020sum']},
+                        {name: '日库存', value: dataMap.dataTI['2020sum']}
+                    ]}
+                ]
+            }
+        ]
+    };
+    //使用制定的配置项和数据显示图表
+    myChart.setOption(option);
+}
