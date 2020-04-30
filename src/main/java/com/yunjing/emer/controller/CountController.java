92ea321d50d974bf04f25b2b276091137df48bc1
1271483191 <1271483191@qq.com>
2020-04-30 14:39:09 +0800

    解决了国家级以下应急网点信息统计的效率问题

--- src/main/java/com/yunjing/emer/controller/CountController.java
+++ src/main/java/com/yunjing/emer/controller/CountController.java
@@ -26,6 +26,9 @@ public class CountController {
     private WebsiteDao websiteDao;
 
     @Autowired
+    private DeliveryDao deliveryDao;
+
+    @Autowired
     private WebsiteService websiteService;
 
     @Autowired
@@ -37,9 +40,9 @@ public class CountController {
     @Autowired
     private AreasDao areasDao;
 
+    @RequestMapping("/toDeliveryCount")
+    public ModelAndView toDeliveryCount(HttpServletRequest request){
 
-    @RequestMapping("/toSupplyCount")
-    public ModelAndView toSupplyCount(HttpServletRequest request){
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("supply-count");
 
@@ -148,6 +151,199 @@ public class CountController {
 
         CompanyInfo companyall = companyInfoDao.sumByUser(user);
         modelAndView.addObject("companyall", companyall);
+
+        Delivery deliveryall = deliveryDao.sumByUser(user);
+        if(deliveryall == null){
+            deliveryall = new Delivery();
+            deliveryall.setCarNum(0);
+            deliveryall.setDeliveryDay(0.0);
+            deliveryall.setDeliveryDayReal(0.0);
+            deliveryall.setDeliveryNum(0);
+            deliveryall.setRadius(0.0);
+            deliveryall.setWareAbility(0.0);
+            deliveryall.setRange("0");
+        }
+        modelAndView.addObject("deliveryall", deliveryall);
+
+        /*计时结束*/
+        Date date2 = new Date();
+        Long time2 = date2.getTime();
+
+        Long totalTime = time2 - time1;
+
+
+        System.out.println("总用时:" + totalTime);
+        System.out.println("查询企业记录数:" + total);
+
+        return modelAndView;
+    }
+
+
+    @RequestMapping("/toSupplyCount")
+    public ModelAndView toSupplyCount(HttpServletRequest request){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("supply-count");
+
+        /*计时*/
+        Date date1 = new Date();
+        Long time1 = date1.getTime();
+        long total = 0;
+
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+
+        StringBuffer promap = new StringBuffer();
+        promap.append("[");
+        int type = user.getType();
+        if(type == 0){
+
+            List<Provinces> list = new ArrayList<Provinces>();
+            ProvincesExample provexample = new ProvincesExample();
+            list = provincesDao.selectByExample(provexample);
+            User myUser = new User();
+            myUser.setType(3);
+
+            for (int i = 0; i < list.size(); i++) {
+
+                promap.append("{'name':'"+list.get(i).getName()+"',"+ "'pId':0,'id':" + list.get(i).getCode() + ",type:'0'},");
+                List<Cities> list2 = new ArrayList<Cities>();
+                CitiesExample citiesExample = new CitiesExample();
+                citiesExample.createCriteria().andProvincecodeEqualTo(list.get(i).getCode());
+                list2 = citiesDao.selectByExample(citiesExample);
+
+
+                for (int j = 0; j < list2.size(); j++) {
+
+                    promap.append("{'name':'"+list2.get(j).getName()+"',"+ "'pId':"+list2.get(j).getProvincecode()+",'id':" + list2.get(j).getCode() + ",type:'1'},");
+                    List<Areas> list3 = new ArrayList<Areas>();
+                    AreasExample areasExample = new AreasExample();
+                    areasExample.createCriteria().andCitycodeEqualTo(list2.get(j).getCode());
+                    list3 = areasDao.selectByExample(areasExample);
+
+                    for (int k = 0; k < list3.size(); k++) {
+
+
+                        myUser.setProvince(list.get(i).getName());
+                        myUser.setCity(list2.get(j).getName());
+                        myUser.setCounty(list3.get(k).getName());
+                        List<Website> list4 = websiteService.selectWebsiteByCounty(myUser);
+                        if(list4.size() > 0){
+
+                        promap.append("{'name':'"+list3.get(k).getName()+"',"+ "'pId':"+list3.get(k).getCitycode()+",'id':" + list3.get(k).getCode() + ",type:'2'},");
+                        }
+                        for(int l = 0; l < list4.size(); l++){
+                            CompanyInfo companyInfo = companyInfoDao.selectByPrimaryKey(list4.get(l).getCompanyId());
+                            promap.append("{'name':'"+companyInfo.getName()+"',"+ "'pId':"+list3.get(k).getCode()+",'id':" + list4.get(l).getCompanyId() + ",type:'3'},");
+                            System.out.println(companyInfo);
+                            total++;
+                        }
+
+                    }
+                }
+            }
+        }
+        if(type == 1){
+            User myUser = new User();
+            myUser.setType(3);
+            List<Provinces> list = new ArrayList<Provinces>();
+            ProvincesExample provexample = new ProvincesExample();
+            provexample.createCriteria().andNameEqualTo(user.getProvince());
+            list = provincesDao.selectByExample(provexample);
+            promap.append("{'name':'"+list.get(0).getName()+"',"+ "'pId':0,'id':" + list.get(0).getCode() + ",type:'0'},");
+            List<Cities> list2 = new ArrayList<Cities>();
+            CitiesExample citiesExample = new CitiesExample();
+            citiesExample.createCriteria().andProvincecodeEqualTo(list.get(0).getCode());
+            list2 = citiesDao.selectByExample(citiesExample);
+            for (int j = 0; j < list2.size(); j++) {
+                promap.append("{'name':'"+list2.get(j).getName()+"',"+ "'pId':"+list.get(0).getCode()+",'id':" + list2.get(j).getCode() + ",type:'1'},");
+                List<Areas> list3 = new ArrayList<Areas>();
+                AreasExample areasExample = new AreasExample();
+                areasExample.createCriteria().andCitycodeEqualTo(list2.get(j).getCode());
+                list3 = areasDao.selectByExample(areasExample);
+                for (int k = 0; k < list3.size(); k++) {
+                    promap.append("{'name':'"+list3.get(k).getName()+"',"+ "'pId':"+list2.get(j).getCode()+",'id':" + list3.get(k).getCode() + ",type:'2'},");
+                    myUser.setProvince(user.getProvince());
+                    myUser.setCity(list2.get(j).getName());
+                    myUser.setCounty(list3.get(k).getName());
+                    List<Website> list4 = websiteService.selectWebsiteByCounty(myUser);
+                    for(int l = 0; l < list4.size(); l++){
+                        CompanyInfo companyInfo = companyInfoDao.selectByPrimaryKey(list4.get(l).getCompanyId());
+                        promap.append("{'name':'"+companyInfo.getName()+"',"+ "'pId':"+list3.get(k).getCode()+",'id':" + list4.get(l).getCompanyId() + ",type:'3'},");
+                        System.out.println(companyInfo);
+                        total++;
+                    }
+                }
+            }
+        }
+        if(type == 2){
+            User myUser = new User();
+            myUser.setType(3);
+            List<Cities> list2 = new ArrayList<Cities>();
+            CitiesExample citiesExample = new CitiesExample();
+            citiesExample.createCriteria().andNameEqualTo(user.getCity());
+            list2 = citiesDao.selectByExample(citiesExample);
+            promap.append("{'name':'"+list2.get(0).getName()+"',"+ "'pId':0,'id':" + list2.get(0).getCode() + ",type:'1'},");
+            List<Areas> list3 = new ArrayList<Areas>();
+            AreasExample areasExample = new AreasExample();
+            areasExample.createCriteria().andCitycodeEqualTo(list2.get(0).getCode());
+            list3 = areasDao.selectByExample(areasExample);
+            for (int k = 0; k < list3.size(); k++) {
+                promap.append("{'name':'"+list3.get(k).getName()+"',"+ "'pId':"+list3.get(k).getCitycode()+",'id':" + list3.get(k).getCode() + ",type:'2'},");
+                myUser.setProvince(user.getProvince());
+                myUser.setCity(user.getCity());
+                myUser.setCounty(list3.get(k).getName());
+                List<Website> list4 = websiteService.selectWebsiteByCounty(myUser);
+                for(int l = 0; l < list4.size(); l++){
+                    CompanyInfo companyInfo = companyInfoDao.selectByPrimaryKey(list4.get(l).getCompanyId());
+                    promap.append("{'name':'"+companyInfo.getName()+"',"+ "'pId':"+list3.get(k).getCode()+",'id':" + list4.get(l).getCompanyId() + ",type:'3'},");
+                    System.out.println(companyInfo);
+                    total++;
+                }
+            }
+        }
+        if(type == 3){
+            User myUser = new User();
+            myUser.setType(3);
+            List<Areas> list3 = new ArrayList<Areas>();
+            AreasExample areasExample = new AreasExample();
+            areasExample.createCriteria().andNameEqualTo(user.getCounty());
+            list3 = areasDao.selectByExample(areasExample);
+            promap.append("{'name':'"+list3.get(0).getName()+"',"+ "'pId':0,'id':" + list3.get(0).getCode() + ",type:'2'},");
+            myUser.setProvince(user.getProvince());
+            myUser.setCity(user.getCity());
+            myUser.setCounty(user.getCounty());
+            List<Website> list4 = websiteService.selectWebsiteByCounty(myUser);
+            for(int l = 0; l < list4.size(); l++){
+                CompanyInfo companyInfo = companyInfoDao.selectByPrimaryKey(list4.get(l).getCompanyId());
+                promap.append("{'name':'"+companyInfo.getName()+"',"+ "'pId':" + list3.get(0).getCode() + ",'id':" + list4.get(l).getCompanyId() + ",type:'3'},");
+                System.out.println(companyInfo);
+                total++;
+            }
+        }
+
+
+
+
+
+
+
+        promap.append("]");
+        System.out.println(promap);
+        modelAndView.addObject("promap", promap.toString());
+        List<CompanyInfo> companys = companyInfoDao.selectCompanyInfoByLevel(user);
+        modelAndView.addObject("companys", companys);
+
+        CompanyInfo companyall = companyInfoDao.sumByUser(user);
+        if(companyall == null){
+            companyall = new CompanyInfo();
+            companyall.setInDay(0.0);
+            companyall.setOutDay(0.0);
+            companyall.setSaveDay(0.0);
+        }
+        modelAndView.addObject("companyall", companyall);
+
+        System.out.println(user);
+        System.out.println("companyall:" + companyall);
 
         Website websiteall = websiteDao.sumByUser(user);
         if(websiteall == null){
@@ -299,6 +495,8 @@ public class CountController {
             modelAndView.addObject("websiteall", websiteall);
             System.out.println(3);
         }
+
+        System.out.println(promap);
 
         modelAndView.addObject("promap", promap);
 
